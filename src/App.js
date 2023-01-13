a96a098a9491bffc11357eaf2141c2c7d0fe35f8
ileanachamorro <ic.chamorro07@gmail.com>
2023-01-13 17:57:48 -0300

    Estado y Propiedades

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import logo from './logo.svg';
 import Componente from "./components/Componente";
 import './App.css';
-
+import Propiedades from "./components/Propiedades";
+import Estado from "./components/Estado";
 function App() {
   let nombre = "Ileana";
   let auth = false;
@@ -10,20 +11,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <section>
         <img src={logo} className="App-logo" alt="logo" />
-        <label htmlFor="nombre">Nombre</label>
-        <input type="text" id="nombre"></input>
-        <h1>{nombre}</h1>
-        <p>{auth ? "El usuario esta logueado" : "El usuario NO esta logueado"}</p>
         <p>
           Editar <code>src/App.js</code> and save to reload.
         </p>
-        <p>{12+1}</p>
-        <ul>
-          {estaciones.map((el, index) => (
-          <li key={index}>{el}</li>
-          ))}
-        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -32,13 +24,27 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+        </section> 
       <section>
         <Componente msg="Hola soy un Componente Funcional Expresado desde una prop">
           <br/>
         </Componente>
+        <hr/>
+        <Propiedades cadena="String" 
+        numero = "19"
+        booleano = {true}
+        arreglo={[1,2,3]}
+        objeto={{nombre: "Ileana", correo: "ile@mail.com"}}
+        funcion={(num) => num * num}
+        elementoReact={<i>Esto es un Elemento React</i>}
+        componenteReact={<Componente msg="Soy un componente pasado como Propiedad"/>}
+        />
+        <hr/>
+        <Estado/>
       </section>
+      </header>
     </div>
+    
   );
 }
 
---
+++ src/components/Estado.js
@@ -0,0 +1,39 @@
+import React, {Component} from "react";
+
+/*Componente funcional */
+function EstadoalHijo(props){
+    return(
+        <div>
+            <h3>
+                {props.contadorHijo}
+            </h3>
+        </div>
+    )
+}
+export default class Estado extends Component{
+    /*Objeto del estado */
+    constructor(props){
+        super(props);
+        this.state = {
+            contador: 0,
+        };
+    
+        
+        setInterval(() => {
+            this.setState({
+                contador: this.state.contador + 1
+            });
+        }, 1000);
+
+    }
+    
+    render(){
+        return(
+            <div>
+                <h2>El State</h2>
+                <p>{this.state.contador}</p>
+                <EstadoalHijo contadorHijo={this.state.contador}/>
+            </div>
+        )
+    }
+}

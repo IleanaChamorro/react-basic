77a9512b80b7d6b09e49f00adcfcec88e4aa325a
ileanachamorro <ic.chamorro07@gmail.com>
2023-02-18 23:44:34 -0300

    Styled Component

--- src/components/ComponentesEstilizados.js
+++ src/components/ComponentesEstilizados.js
@@ -2,6 +2,10 @@ import React from 'react';
 import styled from "styled-components";
 
 export default function ComponentesEstilizados(){
+    const MyH3 = styled.h3`
+    padding: 2rem;
+    text-align: center;
+    `; 
     return(
         <>
         <h2>Styled-Components</h2>
--- src/components/Estilos.js
+++ src/components/Estilos.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import "./Estilos.css";
 
 export default function Estilos(){
     return(
         <section className="estilos">
             <h2>Estilos en React</h2>
+            <h3 className="bg-react">Estilos en hoja CSS Externa</h3>
         </section>
     )
 }
